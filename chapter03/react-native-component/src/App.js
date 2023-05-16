8ed526f94b85b0e0adec1f9eed8e536b6903ae40
LeeSooo <dltn990930@naver.com>
2023-05-16 09:42:10 +0900

    chore: 3장 - 컴포넌트 (2023-05-16)
    해당 내용을 복습함.

--- chapter03/react-native-component/src/App.js
+++ chapter03/react-native-component/src/App.js
@@ -21,12 +21,13 @@ const App = () => {
 
 export default App;
 
-/*
-// 3.3.2. state
-import React from 'react';
-import { View } from 'react-native';
-import Counter from './components/Counter';
+// 3-1. JSX
+/**/
 
+// 3-2. (1) 핵심 컴포넌트 
+/*
+import React from 'react';
+import { Text, View, Button } from 'react-native';
 const App = () => {
   return (
     <View
@@ -37,49 +38,41 @@ const App = () => {
         justifyContent: 'center',
       }}
     >
-      <Counter />
+      <Text style={{ fontSize: 30, marginBottom: 10 }}>Button Component</Text>
+      <Button title="Button" onPress={() => alert('Click !!!')} />
     </View>
   );
 };
-
 export default App;
 */
 
+// 3-2. (2) 내장 컴포넌트
 /*
-// 3.3.1. props
-import React from 'react';
-import { Text, View } from 'react-native';
-import MyButton from './components/MyButton';
-
+import React from "react";
+import { Text, View, Button } from "react-native";
 const App = () => {
   return (
     <View
       style={{
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
-      <Text style={{ fontSize: 30, marginBottom: 10 }}>Props</Text>
-      <MyButton title="Button" onPress={() => alert('props')} />
-      <MyButton title="Button" onPress={() => alert('children')}>
-        Children Props
-      </MyButton>
-      <MyButton onPress={() => alert('default')} />
+      <Text style={{ fontSize: 30, marginBottom: 10 }}>Button Component</Text>
+      <Button title="Button" onPress={() => alert("click!")} />
     </View>
   );
 };
-
 export default App;
 */
 
+// 3-2. (3) 커스텀 컴포넌트
 /*
-// 3.2.2. 나만의 컴포넌트 만들기
 import React from 'react';
 import { Text, View } from 'react-native';
 import MyButton from './components/MyButton';
-
 const App = () => {
   return (
     <View
@@ -102,15 +95,14 @@ const App = () => {
     </View>
   );
 };
-
 export default App;
 */
 
+// 3-3. (1) props
 /*
-// 3.2.1. 핵심 컴포넌트
 import React from 'react';
-import { Text, View, Button } from 'react-native';
-
+import { Text, View } from 'react-native';
+import MyButton from './components/MyButton';
 const App = () => {
   return (
     <View
@@ -121,11 +113,36 @@ const App = () => {
         justifyContent: 'center',
       }}
     >
-      <Text style={{ fontSize: 30, marginBottom: 10 }}>Button Component</Text>
-      <Button title="Button" onPress={() => alert('Click !!!')} />
+      <Text style={{ fontSize: 30, marginBottom: 10 }}>Props</Text>
+      <MyButton title="Button" onPress={() => alert('props')} />
+      <MyButton title="Button" onPress={() => alert('children')}>
+        Children Props
+      </MyButton>
+      <MyButton onPress={() => alert('default')} />
     </View>
   );
 };
+export default App;
+*/
 
+// 3-3. (2) state
+/*
+import React from 'react';
+import { View } from 'react-native';
+import Counter from './components/Counter';
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Counter />
+    </View>
+  );
+};
 export default App;
 */
