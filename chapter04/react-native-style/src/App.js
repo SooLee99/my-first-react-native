ac9fe2fa7c01dc008af48aa9a39b8564db7e15e5
LeeSooo <dltn990930@naver.com>
2023-05-17 09:42:10 +0900

    chore: 4장 - 스타일 (2023-05-17)
    해당 내용을 복습함.

--- chapter04/react-native-style/src/App.js
+++ chapter04/react-native-style/src/App.js
@@ -31,57 +31,40 @@ const App = () => {
 
 export default App;
 
+
+// 4-1. (1) 인라인 스타일링
 /*
-// 4.2.3. 그림자
-import React from 'react';
-import { View } from 'react-native';
-import { viewStyles } from './styles';
-import ShadowBox from './components/ShadowBox';
-
-const App = () => {
-  return (
-    <View style={viewStyles.container}>
-      <ShadowBox />
-    </View>
-  );
-};
-
-export default App;
-*/
-
-/*
-// 4.2.1. flex와 범위
-import React from 'react';
-import { View } from 'react-native';
-import { viewStyles } from './styles';
-import { Header, Contents, Footer } from './components/Layout';
-
-const App = () => {
-  return (
-    <View style={viewStyles.container}>
-      <Header />
-      <Contents />
-      <Footer />
-    </View>
-  );
-};
-
-export default App;
-*/
-
-/*
-// 4.1.4. 외부 스타일 이용하기
 import React from 'react';
 import { View, Text } from 'react-native';
-import { viewStyles, textStyles } from './styles';
 
 const App = () => {
   return (
-    <View style={viewStyles.container}>
-      <Text style={[textStyles.text, { color: 'green' }]}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text
+        style={{
+          padding: 10,
+          fontSize: 26,
+          fontWeight: '600',
+          color: 'black',
+        }}
+      >
         Inline Styling - Text
       </Text>
-      <Text style={[textStyles.text, textStyles.error]}>
+      <Text
+        style={{
+          padding: 10,
+          fontSize: 26,
+          fontWeight: '400',
+          color: 'red',
+        }}
+      >
         Inline Styling - Error
       </Text>
     </View>
@@ -91,8 +74,9 @@ const App = () => {
 export default App;
 */
 
+
+// 4-1. (3) 여러 개의 스타일 적용
 /*
-// 4.1.3 여러 개의 스타일 적용
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
@@ -129,41 +113,62 @@ const styles = StyleSheet.create({
 export default App;
 */
 
+
+// 4-1. (4) 외부 스타일 이용하기
 /*
-// 4.1.1. 인라인 스타일링
 import React from 'react';
 import { View, Text } from 'react-native';
+import { viewStyles, textStyles } from './styles';
 
 const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Text
-        style={{
-          padding: 10,
-          fontSize: 26,
-          fontWeight: '600',
-          color: 'black',
-        }}
-      >
+    <View style={viewStyles.container}>
+      <Text style={[textStyles.text, { color: 'green' }]}>
         Inline Styling - Text
       </Text>
-      <Text
-        style={{
-          padding: 10,
-          fontSize: 26,
-          fontWeight: '400',
-          color: 'red',
-        }}
-      >
+      <Text style={[textStyles.text, textStyles.error]}>
         Inline Styling - Error
       </Text>
+    </View>
+  );
+};
+
+export default App;
+*/
+
+
+// 4-2. (1) flex와 범위
+/*
+import React from 'react';
+import { View } from 'react-native';
+import { viewStyles } from './styles';
+import { Header, Contents, Footer } from './components/Layout';
+
+const App = () => {
+  return (
+    <View style={viewStyles.container}>
+      <Header />
+      <Contents />
+      <Footer />
+    </View>
+  );
+};
+
+export default App;
+*/
+
+
+// 4-2 (3) 그림자
+/*
+import React from 'react';
+import { View } from 'react-native';
+import { viewStyles } from './styles';
+import ShadowBox from './components/ShadowBox';
+
+const App = () => {
+  return (
+    <View style={viewStyles.container}>
+      <ShadowBox />
     </View>
   );
 };
